729f816650b99eaf2953952763b98bb644d8b981
Wilson Lin <linwx1978@yahoo.com>
2026-04-26 19:30:27 +0000

    fix: add title prop to About container

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux'
 import Article from '../../components/Article'
+import AboutData from '../../static/About.json'
 
 export default connect(
     state => ({
-        message: state.about.text
+        message: state.about.text,
+        title: AboutData.title
     })
 )(Article)
